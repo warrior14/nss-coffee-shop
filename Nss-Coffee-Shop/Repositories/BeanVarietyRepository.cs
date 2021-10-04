c76f7f6a2cf5bceecbcb2810602b26b12e3d1a59
warrior14 <ironaddicts14@gmail.com>
2021-10-04 13:29:15 -0500

    created a model and repository folder

---
+++ Nss-Coffee-Shop/Repositories/BeanVarietyRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nss_Coffee_Shop.Repositories
+{
+    public class BeanVarietyRepository
+    {
+    }
+}
